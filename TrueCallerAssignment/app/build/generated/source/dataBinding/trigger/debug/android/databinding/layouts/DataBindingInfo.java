959852df572168cae1ffbe93be7e8a9847b8d8e8
Abhijit Chakra <achakra21@gmail.com>
2019-12-26 22:31:16 +0530

    spelling correction in build.gradle

--- TrueCallerAssignment/app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
+++ TrueCallerAssignment/app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="22e1c23d-afa9-4944-977a-8f1d307ff5eb")
+@BindingBuildInfo(buildId="82c21e5a-6285-4ba4-b359-c3adcdfc0f5b")
 public class DataBindingInfo {}
--- TrueCallerAssignment/app/build/tmp/kapt3/stubs/debug/com/abhijit/truecaller_assingment/ui/mainui/MainActivity.java
+++ TrueCallerAssignment/app/build/tmp/kapt3/stubs/debug/com/abhijit/truecaller_assingment/ui/mainui/MainActivity.java
@@ -2,13 +2,13 @@ package com.abhijit.truecaller_assingment.ui.mainui;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 1, 10}, bv = {1, 0, 2}, k = 1, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u0003\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u00012\u00020\u0004B\u0005\u00a2\u0006\u0002\u0010\u0005J\u0010\u0010\u000f\u001a\u00020\u00102\u0006\u0010\u0011\u001a\u00020\u0012H\u0016J\r\u0010\u0013\u001a\u00020\u0002H\u0016\u00a2\u0006\u0002\u0010\u0014J\b\u0010\u0015\u001a\u00020\u0016H\u0016J\b\u0010\u0017\u001a\u00020\u0003H\u0016J\u0012\u0010\u0018\u001a\u00020\u00102\b\u0010\u0019\u001a\u0004\u0018\u00010\u001aH\u0014R\u0010\u0010\u0006\u001a\u00020\u0002X\u0082\u000e\u00a2\u0006\u0004\n\u0002\u0010\u0007R\u0010\u0010\b\u001a\u0004\u0018\u00010\u0003X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u001e\u0010\t\u001a\u00020\n8\u0006@\u0006X\u0087.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000b\u0010\f\"\u0004\b\r\u0010\u000e\u00a8\u0006\u001b"}, d2 = {"Lcom/abhijit/truecaller_assingment/ui/mainui/MainActivity;", "Lcom/abhijit/truecaller_assingment/ui/rootui/RootActivity;", "error/NonExistentClass", "Lcom/abhijit/truecaller_assingment/ui/mainui/MainActivityModel;", "Lcom/abhijit/truecaller_assingment/ui/mainui/MainActivityNavigator;", "()V", "mActivityMainBinding", "Lerror/NonExistentClass;", "mMainActivityModel", "mViewModelFactory", "Landroid/arch/lifecycle/ViewModelProvider$Factory;", "getMViewModelFactory", "()Landroid/arch/lifecycle/ViewModelProvider$Factory;", "setMViewModelFactory", "(Landroid/arch/lifecycle/ViewModelProvider$Factory;)V", "errorOccured", "", "throwable", "", "getBindingVariable", "()Lerror/NonExistentClass;", "getLayoutId", "", "getViewModel", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
-public final class MainActivity extends com.abhijit.truecaller_assingment.ui.rootui.RootActivity<error.NonExistentClass, com.abhijit.truecaller_assingment.ui.mainui.MainActivityModel> implements com.abhijit.truecaller_assingment.ui.mainui.MainActivityNavigator {
+@kotlin.Metadata(mv = {1, 1, 10}, bv = {1, 0, 2}, k = 1, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u0003\n\u0000\n\u0002\u0010\b\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u00012\u00020\u0004B\u0005\u00a2\u0006\u0002\u0010\u0005J\u0010\u0010\u000e\u001a\u00020\u000f2\u0006\u0010\u0010\u001a\u00020\u0011H\u0016J\b\u0010\u0012\u001a\u00020\u0013H\u0016J\b\u0010\u0014\u001a\u00020\u0013H\u0016J\b\u0010\u0015\u001a\u00020\u0003H\u0016J\u0012\u0010\u0016\u001a\u00020\u000f2\b\u0010\u0017\u001a\u0004\u0018\u00010\u0018H\u0014R\u0010\u0010\u0006\u001a\u0004\u0018\u00010\u0002X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u0007\u001a\u0004\u0018\u00010\u0003X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u001e\u0010\b\u001a\u00020\t8\u0006@\u0006X\u0087.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\n\u0010\u000b\"\u0004\b\f\u0010\r\u00a8\u0006\u0019"}, d2 = {"Lcom/abhijit/truecaller_assingment/ui/mainui/MainActivity;", "Lcom/abhijit/truecaller_assingment/ui/rootui/RootActivity;", "Lcom/abhijit/truecaller_assingment/databinding/ActivityMainBinding;", "Lcom/abhijit/truecaller_assingment/ui/mainui/MainActivityModel;", "Lcom/abhijit/truecaller_assingment/ui/mainui/MainActivityNavigator;", "()V", "mActivityMainBinding", "mMainActivityModel", "mViewModelFactory", "Landroid/arch/lifecycle/ViewModelProvider$Factory;", "getMViewModelFactory", "()Landroid/arch/lifecycle/ViewModelProvider$Factory;", "setMViewModelFactory", "(Landroid/arch/lifecycle/ViewModelProvider$Factory;)V", "errorOccured", "", "throwable", "", "getBindingVariable", "", "getLayoutId", "getViewModel", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class MainActivity extends com.abhijit.truecaller_assingment.ui.rootui.RootActivity<com.abhijit.truecaller_assingment.databinding.ActivityMainBinding, com.abhijit.truecaller_assingment.ui.mainui.MainActivityModel> implements com.abhijit.truecaller_assingment.ui.mainui.MainActivityNavigator {
     @org.jetbrains.annotations.NotNull()
     @javax.inject.Inject()
     public android.arch.lifecycle.ViewModelProvider.Factory mViewModelFactory;
     private com.abhijit.truecaller_assingment.ui.mainui.MainActivityModel mMainActivityModel;
-    private error.NonExistentClass mActivityMainBinding;
+    private com.abhijit.truecaller_assingment.databinding.ActivityMainBinding mActivityMainBinding;
     private java.util.HashMap _$_findViewCache;
     
     @org.jetbrains.annotations.NotNull()
@@ -31,10 +31,9 @@ public final class MainActivity extends com.abhijit.truecaller_assingment.ui.roo
         return null;
     }
     
-    @org.jetbrains.annotations.NotNull()
     @java.lang.Override()
-    public error.NonExistentClass getBindingVariable() {
-        return null;
+    public int getBindingVariable() {
+        return 0;
     }
     
     @java.lang.Override()
